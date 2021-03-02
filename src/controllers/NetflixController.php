0ddc4843613a9efbda2f07c1e482361f67e450f3
Vickie <vcamilaxs@gmail.com>
2021-03-02 15:41:26 -0300

    correções

--- src/controllers/NetflixController.php
+++ src/controllers/NetflixController.php
@@ -9,7 +9,8 @@ class NetflixController extends Conn
 
     private function randomNumber()
     {
-        $count = mysqli_query($this->connect(), "SELECT COUNT(*) AS count FROM netflix");
+        $connect = $this->connect();
+        $count = mysqli_query($connect, "SELECT COUNT(*) AS count FROM netflix");
         $count = mysqli_fetch_assoc($count);
 
         $count = rand(1, $count['count']);
@@ -19,7 +20,8 @@ class NetflixController extends Conn
 
     public function randomRegister()
     {
-        $register = mysqli_query($this->connect(), "SELECT type, title, director, cast1 AS cast, country, date_added, release_year, rating, duration, listed_in, description FROM netflix WHERE id = " . $this->randomNumber());
+        $connect = $this->connect();
+        $register = mysqli_query($connect, "SELECT type, title, director, cast1 AS cast, country, date_added, release_year, rating, duration, listed_in, description FROM netflix WHERE id = " . $this->randomNumber());
 
         $register = mysqli_fetch_assoc($register);
 
